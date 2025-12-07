21eca5fa52bd782bf2c19f1e2133cf59a5e504ad
Alexis <alexisrp_4@hotmail.com>
2025-12-06 18:33:44 -0600

    Create function that repeats string n given times

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,20 @@
-const repeatString = function() {
+const repeatString = function(text, numRep) {
 
+    if(numRep >= 0)
+    {
+        let fullText = ""
+
+        for(let i= 0; i< numRep; i++)
+        {
+            fullText += text;
+        }
+
+        return fullText;
+    }
+    else
+    {
+        return "ERROR";
+    }
 };
 
 // Do not edit below this line
