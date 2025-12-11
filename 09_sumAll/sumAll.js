064c3bb2a3b9cdb4d4f61bbde964d494db10e4b3
Alexis <alexisrp_4@hotmail.com>
2025-12-10 18:51:50 -0600

    Implement function that sum integers in a range
    The function takes two positive integer numbers
    and returns the sum of every integer between and
    including them.

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,4 +1,37 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+
+    if(typeof a === "number" & typeof b === "number")
+    {
+        if(a > -1 & b > -1)
+        {
+            if(Number.isInteger(a) & Number.isInteger(b))
+            {
+                let total= 0;
+
+                let start = a > b? b : a;
+                let end = a > b? a : b;
+
+                for(let i= start; i<=end; i++)
+                {
+                    total += i;
+                }
+
+                return total;
+            }
+            else
+            {
+                return "ERROR";
+            }
+        }
+        else
+        {
+            return "ERROR";
+        }
+    }
+    else
+    {
+        return "ERROR";
+    }
 
 };
 
